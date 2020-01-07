6b66f162c843653c5712a9d49a97e7d26b2fcc94
julienBartholini <julienbartholini.pro@gmail.com>
2020-01-07 09:18:56 +0100

    EXERCICES WEB : Fil Rouge / Squelette balises sémentique

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const main = document.querySelector('main'),
-    nomsExos = ['JCVD et Co'];
+    nomsExos = ['JCVD et Co','','','','','','','','','','','','','','','','','','','FIL ROUGE'];
 
 for (let i = 0; i < nomsExos.length; i++) {
     const lien = document.createElement('a');
-    
-    lien.href = `exercices/ex${i + 1}/index.html`;
-    lien.target = '_blank';
-    
-    lien.textContent = `EXERCICE ${i + 1} : ${nomsExos[i]}`;
-    
+    if (nomsExos[i] !== '') {
+        lien.href = `exercices/ex${i + 1}/index.html`;
+        lien.textContent = `EXERCICE ${i + 1} : ${nomsExos[i]}`;
+        lien.target = '_blank';
+    } else {
+        lien.href = '#';
+        lien.textContent = `INDISPONIBLE`;
+        lien.style.opacity = .25;
+    }
     main.appendChild(lien);
 }

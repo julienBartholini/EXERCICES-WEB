5d81a51f889457ab7fea11836e20274322b8133e
julienBartholini <julienbartholini.pro@gmail.com>
2020-01-23 18:45:28 +0100

    WEB EXERCICES : Ex4 Clean & Bonus

---
+++ exercices/ex4/scripts/script.js
@@ -0,0 +1,6 @@
+const main = document.querySelector('main'),
+    btn = document.querySelector('button');
+
+btn.addEventListener('click', function () {
+  main.style.animation = 'groovy 5s ease infinite';
+});

8c319bcbed4e1e6f35526871cf31d9f20bb2a32a
julienBartholini <julienbartholini.pro@gmail.com>
2020-01-27 10:18:55 +0100

    WEB EXERCICES : Master Ex Introduction Page Complete

--- exercices/ex20/scripts/script.js
+++ exercices/ex20/scripts/script.js
@@ -1,6 +1,27 @@
+function openIntro() {
+  if (opacityIntro < 1) {
+    opacityIntro += .01;
+    sectionIntro.style.opacity = opacityIntro;
+    requestAnimationFrame(openIntro);
+  } else {
+    cancelAnimationFrame(requestAnimationFrame(openIntro));
+  }
+}
+
+function closeIntro() {
+  if (opacityIntro > 0) {
+    opacityIntro -= .025;
+    sectionIntro.style.opacity = opacityIntro;
+    requestAnimationFrame(closeIntro);
+  } else {
+    sectionIntro.style.display = 'none';
+    cancelAnimationFrame(requestAnimationFrame(closeIntro));
+  }
+}
+
 function openCarteInfos() {
   if (opacityCard < 1) {
-    opacityCard += .05;
+    opacityCard += .1;
     carteInfos.style.opacity = opacityCard;
     requestAnimationFrame(openCarteInfos);
   } else {
@@ -10,7 +31,7 @@ function openCarteInfos() {
 
 function closeCarteInfos() {
   if (opacityCard > 0) {
-    opacityCard -= .05;
+    opacityCard -= .1;
     carteInfos.style.opacity = opacityCard;
     requestAnimationFrame(closeCarteInfos);
   } else {
@@ -19,18 +40,32 @@ function closeCarteInfos() {
   }
 }
 
-let opacityCard = 0;
+let opacityCard = 0,
+    opacityIntro = 0;
 
 const audio = document.querySelector('audio'),
+    btnCommencer = document.querySelector('#btnCommencer'),
     btnInfos = document.querySelector('#btnInfos'),
+    sectionIntro = document.querySelector('#sectionIntro'),
     carteInfos = document.querySelector('#carteInfos'),
     btnFermer = document.querySelector('#btnFermer');
 
+requestAnimationFrame(openIntro);
+
+btnCommencer.addEventListener('click', function () {
+  audio.src = 'sons/start.mp3';
+  audio.play();
+  requestAnimationFrame(closeIntro);
+});
+
 btnInfos.addEventListener('click', function () {
+  audio.src = 'sons/tic2.mp3';
+  audio.play();
   carteInfos.style.display = 'flex';
   requestAnimationFrame(openCarteInfos);
 });
 
 btnFermer.addEventListener('click', function () {
+  audio.play();
   requestAnimationFrame(closeCarteInfos);
 });

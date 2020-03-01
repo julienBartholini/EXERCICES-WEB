be9f903ff4ad2bedb9ce960dd9bbe006eb436823
julienBartholini <julienbartholini.pro@gmail.com>
2020-03-01 20:15:36 +0100

    WEB EXERCICES : MASTER Ex (Silent Hill) Function 'Scenariste' Created

--- exercices/ex10/scripts/script.js
+++ exercices/ex10/scripts/script.js
@@ -1,4 +1,4 @@
-class Scenariste {
+class Elements {
   constructor(visuel, nom, description, nbPts) {
     this.visuel = visuel;
     this.nom = nom;
@@ -7,6 +7,14 @@ class Scenariste {
   }
   affiche() {
     return `VISUEL : ${this.visuel} / NOM : ${this.nom} / DESCRIP : ${this.description} / PTS : ${this.nbPts}`;
+  }
+}
+
+function senariste(mesCategories, monScenario) {
+  for (let i = 0; i < mesCategories.length; i++) {
+    let iRandom = Math.floor(Math.random() * mesCategories[i].length);
+    monScenario.push(mesCategories[i][iRandom]);
+    console.log(monScenario[i]);
   }
 }
 
@@ -93,7 +101,7 @@ let opacityCard = 0,
     compteur = 0,
 
     // TABLEAUX DES ELTS DU SCENARIO PRE-GENERE (lieu, arme, survivant, monstre)
-    scenarioElts = [];
+    monScenario = [];
 
 const audio = document.querySelector('audio'),
     sectionIntro = document.querySelector('#sectionIntro'),
@@ -115,40 +123,42 @@ const audio = document.querySelector('audio'),
     tabReactions = ['Où suis-je ?...', "?!... Ca pourrait servir.", 'Qui êtes vous ?...', "C'est quoi ça ?!..."];
 
 // OBJETS LIEUX :
-const rues = new Scenariste('rues', 'Les Rues', '...', 25),
-hotel = new Scenariste('hotel', 'Hotel Lakeview', '...', 21),
-ecole = new Scenariste('ecole', 'Ecole Primaire Midwish', '...', 17),
-hopital = new Scenariste('hopital', 'Hopital Alchemia', '...', 13),
-egouts = new Scenariste('egouts', 'Les Egouts', '...', 9),
-prison = new Scenariste('prison', 'La Prison Lacustre', '...', 3),
+const rues = new Elements('rues', 'Les Rues', '...', 25),
+hotel = new Elements('hotel', 'Hotel Lakeview', '...', 21),
+ecole = new Elements('ecole', 'Ecole Primaire Midwish', '...', 17),
+hopital = new Elements('hopital', 'Hopital Alchemia', '...', 13),
+egouts = new Elements('egouts', 'Les Egouts', '...', 9),
+prison = new Elements('prison', 'La Prison Lacustre', '...', 3),
 // OBJETS ARMES :
-colt = new Scenariste('colt', 'Colt', '...', 25),
-fusil = new Scenariste('fusil', 'Fusil', '...', 18),
-pistolet = new Scenariste('pistole', 'Pistolet', '...', 11),
-tuyau = new Scenariste('tuyau', "Tuyau d'Acier", '...', 3),
+colt = new Elements('colt', 'Colt', '...', 25),
+fusil = new Elements('fusil', 'Fusil', '...', 18),
+pistolet = new Elements('pistole', 'Pistolet', '...', 11),
+tuyau = new Elements('tuyau', "Tuyau d'Acier", '...', 3),
 // OBJETS SURVIVANTS :
-harry = new Scenariste('harry', 'Harry Mason', '...', 25),
-heather = new Scenariste('heather', 'Heather Mason', '...', 22),
-henry = new Scenariste('henry', 'Henry Townshend', '...', 19),
-james = new Scenariste('james', 'James Sunderland', '...', 16),
-cybil = new Scenariste('cybil', 'Cybil Bennett', '...', 13),
-douglas = new Scenariste('douglas', 'Douglas Cartland', '...', 10),
-eileen = new Scenariste('eileen', 'Eileen Galvin', '...', 7),
-laura = new Scenariste('laura', 'Laura', '...', 2),
+harry = new Elements('harry', 'Harry Mason', '...', 25),
+heather = new Elements('heather', 'Heather Mason', '...', 22),
+henry = new Elements('henry', 'Henry Townshend', '...', 19),
+james = new Elements('james', 'James Sunderland', '...', 16),
+cybil = new Elements('cybil', 'Cybil Bennett', '...', 13),
+douglas = new Elements('douglas', 'Douglas Cartland', '...', 10),
+eileen = new Elements('eileen', 'Eileen Galvin', '...', 7),
+laura = new Elements('laura', 'Laura', '...', 2),
 // OBJETS MONSTRES
-lyingFigure = new Scenariste('', '', '...', 25),
-mumbler = new Scenariste('', '', '...', 21),
-nightFlutter = new Scenariste('', '', '...', 17),
-nurse = new Scenariste('', '', '...', 13),
-rubberFace = new Scenariste('', '', '...', 9),
-scraper = new Scenariste('', '', '...', 5),
-pyramid = new Scenariste('', '', '...', 1);
+lyingFigure = new Elements('lyingFigure', 'Lying Figure', '...', 25),
+mumbler = new Elements('mumbler', 'Mumbler', '...', 21),
+nightFlutter = new Elements('nightFlutter', 'Night Flutter', '...', 17),
+nurse = new Elements('nurse', 'Nurse', '...', 13),
+rubberFace = new Elements('rubberFace', 'Rubber Face', '...', 9),
+scraper = new Elements('scraper', 'Scraper', '...', 5),
+pyramid = new Elements('pyramid', 'Pyramid Head', '...', 1);
 
 // TABLEAUX DES CATEGORIES :
 const mesLieux = [rues, hotel, ecole, hopital, egouts, prison],
 mesArmes = [colt, fusil, pistolet, tuyau],
 mesSurvivants = [harry, heather, henry, james, cybil, douglas, eileen, laura],
-mesMonstres = [lyingFigure, mumbler, nightFlutter, nurse, rubberFace, scraper, pyramid];
+mesMonstres = [lyingFigure, mumbler, nightFlutter, nurse, rubberFace, scraper, pyramid],
+
+mesCategories = [mesLieux, mesArmes, mesSurvivants, mesMonstres];
 
 console.log('MES LIEUX :');
 for (let i = 0; i < mesLieux.length; i++) {
@@ -169,6 +179,9 @@ console.log('MES MONSTRES :');
 for (let i = 0; i < mesMonstres.length; i++) {
   console.log(mesMonstres[i].affiche());
 }
+
+// APPEL DE LA FONCTION 'SCENARISTE' :
+senariste(mesCategories, monScenario);
 
 requestAnimationFrame(openIntro);
 
